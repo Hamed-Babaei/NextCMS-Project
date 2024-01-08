2ded4206b034d9118efd0421851471932f2246d9
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-01-09 00:59:09 +0330

    in api's page If logic changed to switch case

--- next-cms/pages/api/courses/index.js
+++ next-cms/pages/api/courses/index.js
@@ -3,30 +3,36 @@ import connectToDB from "@/utils/db";
 
 const handler = async (req, res) => {
   connectToDB();
-  if (req.method === "GET") {
-    try {
-      const courses = await coursesModel.find();
-      return res.status(200).json(courses);
-    } catch (err) {
-      return res
-        .status(500)
-        .json({ message: "UnKnown internal server error !!", err });
-    }
-  } else if (req.method === "POST") {
-    try {
-      const { title } = req.body;
-      if (!title.trim() || title.length < 8) {
-        return res.status(422).json({ message: "Title is not valid !!" });
+  switch (req.method) {
+    case "GET":
+      try {
+        const courses = await coursesModel.find();
+        return res.status(200).json(courses);
+      } catch (err) {
+        return res
+          .status(500)
+          .json({ message: "UnKnown internal server error !!", err });
       }
-      await coursesModel.create({ title });
-      return res
-        .status(201)
-        .json({ message: "Course created successfully :))" });
-    } catch (err) {
-      return res
-        .status(500)
-        .json({ message: "UnKnown internal server error !!" });
-    }
+      break;
+    case "POST":
+      try {
+        const { title } = req.body;
+        if (!title.trim() || title.length < 8) {
+          return res.status(422).json({ message: "Title is not valid !!" });
+        }
+        await coursesModel.create({ title });
+        return res
+          .status(201)
+          .json({ message: "Course created successfully :))" });
+      } catch (err) {
+        return res
+          .status(500)
+          .json({ message: "UnKnown internal server error !!" });
+      }
+      break;
+    default:
+      return res.status(405).end("Method Not Allowed");
+      break;
   }
 };
 
